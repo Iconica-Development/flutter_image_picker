7650150cc6d325d9ec209b8951f1293a10d59575
commitimpush <T.Weening@student.han.nl>
2022-08-31 11:52:29 +0200

    improved the modal

--- lib/src/screens/image_picker_ui.dart
+++ lib/src/screens/image_picker_ui.dart
@@ -6,51 +6,60 @@ import 'package:image_picker/image_picker.dart';
 
 class ImagePickerUI {
   final ImagePickerService _imagePickerService = ImagePickerService();
-  final double iconSize = 125;
+  final double iconSize = 150;
 
   Future<Uint8List?> pickImageDialog(BuildContext context) async {
     return await showModalBottomSheet<Uint8List?>(
       context: context,
       builder: (BuildContext context) {
-        return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.image),
-                tooltip: 'Choose Image From Gallery',
-                iconSize: iconSize,
-                onPressed: () =>
-                    _imagePickerService.pickImage(ImageSource.gallery, context),
+        return Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Upload Image",
+            textScaleFactor: 2,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.image),
+                    tooltip: 'Choose Image From Gallery',
+                    iconSize: iconSize,
+                    onPressed: () => _imagePickerService.pickImage(
+                        ImageSource.gallery, context),
+                  ),
+                  const Text("Select file"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
               ),
-              const Text("Select file"),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.camera_alt_rounded),
+                    tooltip: 'Make Image With Camera',
+                    iconSize: iconSize,
+                    onPressed: () => _imagePickerService.pickImage(
+                        ImageSource.camera, context),
+                  ),
+                  const Text("Take a picture"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
             ],
           ),
-
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.camera_alt_rounded),
-                tooltip: 'Make Image With Camera',
-                iconSize: iconSize,
-                onPressed: () =>
-                    _imagePickerService.pickImage(ImageSource.camera, context),
-              ),
-              const Text("Take a picture"),
-            ],
-          ),
-          // ElevatedButton(
-          //   onPressed: () =>
-          //       _imagePickerService.pickImage(ImageSource.gallery, context),
-          //   child: const Text('Gallery'),
-          // ),
-          // const SizedBox(width: 10),
-          // ElevatedButton(
-          //   onPressed: () =>
-          //       _imagePickerService.pickImage(ImageSource.camera, context),
-          //   child: const Text('Camera'),
-          // ),
+          ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("Close")),
         ]);
       },
     );
