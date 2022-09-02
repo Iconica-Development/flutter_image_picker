1ae89ef3cb7678de817dfa3432d7883ff5c0eab9
commitimpush <T.Weening@student.han.nl>
2022-09-02 16:48:45 +0200

    finished tests

--- lib/src/services/image_picker_service.dart
+++ lib/src/services/image_picker_service.dart
@@ -3,13 +3,16 @@ import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerService {
-  final ImagePicker _imagePicker = ImagePicker();
+  ImagePickerService({this.imagePicker});
+
+  ImagePicker? imagePicker;
 
   /// [pickImage] is the function that picks the image and returns it as a [Uint8List].
   /// The function requires [source], an [ImageSource] that's the method of how the image needs to be picked, for example gallery or camera.
   Future<Uint8List?> pickImage(ImageSource source) async {
     var image =
-        await (await _imagePicker.pickImage(source: source))?.readAsBytes();
+        await (await (imagePicker ?? ImagePicker()).pickImage(source: source))
+            ?.readAsBytes();
     return image;
   }
 }
--- test/mocks/image_picker_mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:image_picker/image_picker.dart';
-import 'package:mocktail/mocktail.dart';
-
-class ImagePickerMock extends Mock implements ImagePicker {}
