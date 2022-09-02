4ede413ee49f04b5ae4bbf3e97354d6b3ca37d1f
commitimpush <T.Weening@student.han.nl>
2022-09-02 14:13:58 +0200

    Added UI tests and attempt at service test

---
+++ test/mocks/image_picker_mock.dart
@@ -0,0 +1,4 @@
+import 'package:image_picker/image_picker.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ImagePickerMock extends Mock implements ImagePicker {}
--- test/src/ui/image_picker_ui_test.dart
+++ test/src/ui/image_picker_ui_test.dart
@@ -1,3 +1,62 @@
-main() {
-  
+import 'package:flutter/material.dart';
+import 'package:flutter_image_picker/image_picker.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Image Picker Shows With Normal Theme', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: Material(child: ImagePicker())));
+
+    final titleFinder = find.text(const ImagePickerTheme().title);
+    final makePhotoIconFinder =
+        find.byIcon(const ImagePickerTheme().makePhotoIcon);
+    final makePhotoTextFinder =
+        find.text(const ImagePickerTheme().makePhotoText);
+    final selectImageIconFinder =
+        find.byIcon(const ImagePickerTheme().selectImageIcon);
+    final selectImageTextFinder =
+        find.text(const ImagePickerTheme().selectImageText);
+    final closebuttonTextFinder =
+        find.text(const ImagePickerTheme().closeButtonText);
+
+    expect(titleFinder, findsOneWidget);
+    expect(makePhotoIconFinder, findsOneWidget);
+    expect(makePhotoTextFinder, findsOneWidget);
+    expect(selectImageIconFinder, findsOneWidget);
+    expect(selectImageTextFinder, findsOneWidget);
+    expect(closebuttonTextFinder, findsOneWidget);
+  });
+
+  testWidgets('Image Picker Shows With Custom Theme', (tester) async {
+    String title = "title";
+    IconData makePhotoIcon = Icons.motorcycle;
+    String makePhotoText = "taaaake image";
+    IconData selectImageIcon = Icons.chevron_left;
+    String selectImageText = "seleeeeect image";
+    String closeButtonText = "Close Dialog!";
+
+    await tester.pumpWidget(MaterialApp(
+        home: Material(
+            child: ImagePicker(
+                imagePickerTheme: ImagePickerTheme(
+                    title: title,
+                    makePhotoIcon: makePhotoIcon,
+                    makePhotoText: makePhotoText,
+                    selectImageIcon: selectImageIcon,
+                    selectImageText: selectImageText,
+                    closeButtonText: closeButtonText)))));
+
+    final titleFinder = find.text(title);
+    final makePhotoIconFinder = find.byIcon(makePhotoIcon);
+    final makePhotoTextFinder = find.text(makePhotoText);
+    final selectImageIconFinder = find.byIcon(selectImageIcon);
+    final selectImageTextFinder = find.text(selectImageText);
+    final closebuttonTextFinder = find.text(closeButtonText);
+
+    expect(titleFinder, findsOneWidget);
+    expect(makePhotoIconFinder, findsOneWidget);
+    expect(makePhotoTextFinder, findsOneWidget);
+    expect(selectImageIconFinder, findsOneWidget);
+    expect(selectImageTextFinder, findsOneWidget);
+    expect(closebuttonTextFinder, findsOneWidget);
+  });
 }
